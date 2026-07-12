2c13a4f0b51834995fc0a6b7beec91ee8cf4f602
staivanov <sta_ivanov@icloud.com>
2026-07-12 18:04:34 +0300

    Basic string demo with couple built-in functions.

---
+++ string_demo.py
@@ -0,0 +1,23 @@
+first_name = "stanislav"
+last_name = "ivanov"
+#f-strings
+full_name = f"{first_name} {last_name}"
+print(full_name)
+name = first_name + " " + last_name
+print(name.title())
+
+print("********************")
+petName = "Lisa"
+print(petName)
+print(petName.lower())
+print(petName.upper())
+print(f"\t{petName.title()}")
+print("Pets from last fifteen years: \n\tTomi \n\tLisa \n\tLucky \n\tFabien")
+
+movieName = "Scarface  "
+#strip function remove whitespace only temporarily. There are rstrip and lstrip for removing right and left whitespace.
+movieName = movieName.strip()
+
+
+
+
