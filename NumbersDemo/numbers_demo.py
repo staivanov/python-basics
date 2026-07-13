8997d1f304f7c1154eb3607b1c6ed269c7030851
staivanov <sta_ivanov@icloud.com>
2026-07-13 10:37:09 +0300

    Two new files were created. First one is for demo purpose and second is for very simple exercises

---
+++ NumbersDemo/numbers_demo.py
@@ -0,0 +1,34 @@
+#1. Basic arithmetic operations
+sum = 2 + 3
+print("sum: ",sum)
+subtracted_numbers = 5 - 1
+print("subtracted numbers", subtracted_numbers)
+numbers_multiplication = 6 * 2
+print("numbers multiplication", numbers_multiplication)
+numbers_division = 9 / 3
+print("numbers division", numbers_division)
+numbers_exponents = 3 ** 3
+print("numbers exponents: ", numbers_exponents)
+
+#2. Dividing two integers
+number_one = 9
+number_two = 3
+two_numbers_divided = number_one / number_two
+print("two numbers divided :", two_numbers_divided)
+
+print("**********************************")
+
+#3. Basic arithmetic operations between int and float numbers
+n1 = 6
+n2 = 2.0
+print("Numbers for next demo where one is int and other is float are ", n1, "and", n2)
+print("Int number plus float number is: ", n1 + n2)
+print("Int number multiplied by float is: ", n1 * n2)
+print("Float number pow on int number is: ", n2**n1)
+
+#4. Multiple Assignment
+x, y, z = 0, 0, 0
+
+#5. Contants
+MAX_POWER = 220
+MIN_POWER = 120
---
+++ NumbersDemo/numbers_exercises.py
@@ -0,0 +1,10 @@
+#1. After every basic arithmetic operation the result should be 8
+print(6+2)
+print(17-9)
+print(16/2)
+print(4*2)
+print(2**3)
+
+favorite_number = 9999
+message_with_my_fav_number = f"My favorite number is {favorite_number}"
+print(message_with_my_fav_number)
