387f42ec860fb1f3c506be29803e2fcb0530692c
staivanov <sta_ivanov@icloud.com>
2026-07-14 16:55:47 +0300

    Two new files were created for demo purposes. First part in them is completed.

---
+++ ListsBasics/intro_lists.py
@@ -0,0 +1,17 @@
+#1. List of cars demo
+famous_car_brands = ["Porsche", "BMW", "Audi", "Mercedes", "VW", "Opel", "RUF", "Peugeot", "Renault", "Citroen", "Jaguar"]
+print("All car brands in my list are: ", famous_car_brands)
+print("*******************")
+
+my_fav_car_brand = famous_car_brands[0]
+print("My favorite car brand is", my_fav_car_brand)
+print("*******************")
+
+last_car_brand = famous_car_brands[-1]
+print("The last car brand is: ", last_car_brand)
+print("*******************")
+
+message_to_all_bmw_fans = f"I like very much {famous_car_brands[1]}"
+print(message_to_all_bmw_fans)
+print("*******************")
+#2.
---
+++ ListsBasics/lists_exercises.py
@@ -0,0 +1,13 @@
+#1.
+animals = ["dog", "eagle", "lion", "cat", "bear", "snake"]
+print(animals[0])
+print(animals[1])
+print(animals[2])
+print(animals[3])
+print(animals[4])
+print(animals[5])
+print("*******************")
+message_to_someone= f"Hey, many people like {animals[0]}s"
+print(message_to_someone)
+print("*******************")
+#2.
