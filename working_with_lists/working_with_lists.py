e1f07af4cea6ba4e794ed0b4fd848856aa53f056
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-18 12:21:08 +0300

    Functions like min, max, sum and range were introduced in demo. Solutions of some exercises with list operations.

--- working_with_lists/working_with_lists.py
+++ working_with_lists/working_with_lists.py
@@ -1,3 +1,28 @@
 car_brands = ["Porsche", "BMW", "Mercedes", "Audi", "RUF", "Opel"]
 for car_brand in car_brands:
     print(car_brand)
+print("******************")
+# Making numerical lists
+
+numbers = list(range(1, 7))
+print("List of numbers: ", numbers)
+square_of_numbers = []
+
+for number in numbers:
+    square = number ** 2
+    square_of_numbers.append(square)
+
+print("List of numbers on square from above: ", square_of_numbers)
+print("******************")
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -11]
+min_digit = min(digits)
+max_digit = max(digits)
+sum_of_all_digits = sum(digits)
+print("All digits in the list are", digits)
+print("Min digit is", min_digit)
+print("Max digit is", max_digit)
+print("Sum of all digits in the list is", sum_of_all_digits)
+
+## List Comprehensions
+squares_v2 = [value ** 2 for value in range(1, 11)]
+print("Demo for list comprehension", squares_v2)
--- working_with_lists/working_with_lists_exercises.py
+++ working_with_lists/working_with_lists_exercises.py
@@ -1,3 +1,4 @@
+# 4.1
 pizzas = ["Neapolitana", "Mania", "Capricciosa"]
 
 for pizza in pizzas:
@@ -5,3 +6,43 @@ for pizza in pizzas:
 
 statement_about_pizzas = "I really like these type of pizzas from above. Recommend it to everyone."
 print(statement_about_pizzas)
+print("***********************")
+# 4.2
+raptors = ["eagle", "falcon", "hawk"]
+
+for raptor in raptors:
+    print(f"{raptor} is found on all continents except on Antarctica.")
+
+print("Any of these animal is skilled in hunting!")
+# 4.3
+numbers_to_twenty = list(range(1, 21))
+
+for number in numbers_to_twenty:
+    if number <= 19:
+        print(number, end=", ", flush=True)
+    else:
+        print(number, flush=True)
+print("***********************")
+# 4.4 One million
+one_million = list(range(1, 1000001))
+# 4.5 Summing a Million
+one_million_min = min(one_million)
+print("Min number in one million range of numbers is", one_million_min)
+one_million_max = max(one_million)
+print("Max number in one million range of numbers is", one_million_max)
+sum_of_one_million_range = sum(one_million)
+print("Sum of numbers one to one million is", sum_of_one_million_range)
+print("***********************")
+# 4.6 Odd numbers
+odd_numbers_in_range = list(range(1, 21, 2))
+print("All odd numbers in range from 1 to 20 are", odd_numbers_in_range)
+print("***********************")
+# 4.7 Threes
+n = 3
+number_multiplied_by_n_in_range = list(number * n for number in range(3, 31))
+print(number_multiplied_by_n_in_range)
+print("***********************")
+# 4.8-9 Cubes
+cube_multiplier = 3
+first_ten_cubes = list(number ** cube_multiplier for number in range(1, 11))
+print("Cubes of the first ten numbers are", first_ten_cubes)
