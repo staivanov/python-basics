7951552d16cc2f064dc4012729df66b688dbb4d0
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-18 09:44:23 +0300

    New folder for working with lists plus two files for demo and solutions on exercises.

---
+++ working_with_lists/working_with_lists.py
@@ -0,0 +1,3 @@
+car_brands = ["Porsche", "BMW", "Mercedes", "Audi", "RUF", "Opel"]
+for car_brand in car_brands:
+    print(car_brand)
---
+++ working_with_lists/working_with_lists_exercises.py
@@ -0,0 +1,7 @@
+pizzas = ["Neapolitana", "Mania", "Capricciosa"]
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza")
+
+statement_about_pizzas = "I really like these type of pizzas from above. Recommend it to everyone."
+print(statement_about_pizzas)
