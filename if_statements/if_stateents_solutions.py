9d7b850df234239283c35437f804d870c165dc18
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-19 19:30:12 +0300

    Solutions for the first part of exercises in this chapter are done.

---
+++ if_statements/if_stateents_solutions.py
@@ -0,0 +1,20 @@
+#5.1 Write series of conditional tests.
+car = "Porsche"
+porsche = "Porsche"
+print("Is car == Porsche? I predict True")
+print(car == porsche)
+
+car = "BMW"
+porsche = "Porsche"
+print("Is car == Audi? I predict True")
+print(car == porsche)
+
+car = "Lexus"
+toyota = "Toyota"
+print("Is car == Toyota? I predict True")
+print(car == toyota)
+
+car = "Acura"
+honda = "Honda"
+print("Is car == Acura? I predict True")
+print(not car == honda)
