de3d4e19e9c9963c5192a9b4f2bd32f2e883f900
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-26 18:10:29 +0300

    Full demo is completed.

--- 6.dictionaries/dictionaries_demo.py
+++ 6.dictionaries/dictionaries_demo.py
@@ -72,5 +72,31 @@ all_unique_numbers = set(people_guessed_number.values())
 print("All unique numbers are:")
 for number in all_unique_numbers:
     print(number)
-#11. Set
+
+print("************************************")
+# 11. Set
 programming_languages = {"C++", "C", "Java", "Python", "JavaScript", }
+
+# 12. Nesting
+car_1 = {"brand": "BMW", "model": "760", "engine_type": "twin-turbo V12", "kW": 480, "color": "white", }
+car_2 = {"brand": "BMW", "model": "M5", "engine_type": "twin-turbo v8", "kW": 400, "color": "red", }
+car_3 = {"brand": "BMW", "model": 325, "engine_type": "4-cylinder diesel", "kW": 120, "color": "blue", }
+
+cars = [car_0, car_1, car_2, car_3]
+print("Those are the first two cars on my list of cars.")
+for car in cars[:2]:
+    print(car)
+
+print("************************************")
+# 13. List in a Dictionary
+students_with_favorite_languages = {
+    "Stanislav": ["C++", "C", "Java", "Python", ],
+    "Ivan": ["php", "JavaScript", ],
+    "Yoana": ["JavaScript", "Java", ],
+    "Petar": ["C#", "Java"]
+}
+
+for student, programming_languages in students_with_favorite_languages.items():
+    print(f"{student}")
+    for language in programming_languages:
+        print(f"\t{language}")
