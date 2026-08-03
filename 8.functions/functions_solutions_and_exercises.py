11a88241f9520557bd8be18b52643e42117f2a71
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-03 03:10:49 +0300

    Two new solutions.

--- 8.functions/functions_solutions_and_exercises.py
+++ 8.functions/functions_solutions_and_exercises.py
@@ -5,6 +5,7 @@ def display_message():
 
 display_message()
 print("******************************************")
+
 # 8.2 Favorite Book
 def favorite_book(title):
     """This function prints message with your favorite book on the console."""
@@ -12,3 +13,20 @@ def favorite_book(title):
 
 my_book = "Shogun"
 favorite_book(my_book)
+print("******************************************")
+
+# 8.3 T-Shirt
+def make_shirt(size, text_message):
+    message = f"On your T-shirt have a text \"{text_message}\". size: {size}."
+    print(message)
+
+make_shirt("S", "Offline till I drink my coffee")
+make_shirt(size = "M", text_message = "Bangaranga")
+
+def make_shirt(size = "M", text_message = "I am using Python."):
+    message = f"On your T-shirt have a text \"{text_message}\". size: {size}."
+    print(message)
+
+make_shirt()
+make_shirt(size="L")
+make_shirt("XXL", "Java is cool!")
