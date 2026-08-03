9c9ce5b95d0578a0b0f5d9e03028f5dc23ac2262
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-03 03:10:14 +0300

    Demo for functions with parameters

--- 8.functions/functions_demo.py
+++ 8.functions/functions_demo.py
@@ -1,14 +1,12 @@
 # Simple function  def without parameters.
 def greeting_to_the_world():
     """Print a simple message."""
-    message = "Evil never comes alone!"
+    message = "No no, no no no no, no no no no, no no there's no limit!!!"
     print(message)
-    message = "Fighting the world!"
-    print(message)
-
 
 greeting_to_the_world()
 print("***********************************************")
+
 #Simple function def with parameters.
 
 def calcRectangleArea(length, width):
@@ -18,6 +16,33 @@ def calcRectangleArea(length, width):
 a = 5
 b = 10
 res = calcRectangleArea(a, b)
-print("The are of your rectangle is ", res)
+print("The area of your rectangle is ", res, "cm.")
 print("***********************************************")
+
+# Positional Arguments
+def describe_car(brand, model):
+    """Display information about a car and the model."""
+    print(f"{brand} {model}")
+
+brand = "Porsche"
+model = "911 RS2"
+describe_car(brand, model)
+
+brand_v2 = "Ford"
+model = "F-150 Raptor"
+describe_car(brand_v2, model)
+
+print("***********************************************")
+
+# Keyword Arguments
+describe_car(brand = "BMW", model = "M8")
+
+
+# Default values
+def describe_car(model, brand = "BMW"):
+    """Display information about a car and the model."""
+    print(f"{brand} {model}")
+
+describe_car("335i")
+describe_car("Land Cruiser", brand = "Toyota")
 
