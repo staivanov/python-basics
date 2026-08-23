d6250bc8beb83c54db1c6867380dee1d70d70e5b
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-23 23:41:11 +0300

    Simple demo with dogs instances and restaurants instances.

--- 9.classes/classes_demo.py
+++ 9.classes/classes_demo.py
@@ -1,8 +1,21 @@
 from dog import Dog
+from restaurant import Restaurant
 
+# Dog demo.
 my_dog = Dog("Dark Lord", 8, "male")
 neighbours_dog = Dog("Daisy", 2, "female")
 
 my_dog.present_myself()
 neighbours_dog.present_myself()
 neighbours_dog.sit()
+print('*' * 30)
+
+# Restaurant demo.
+
+trespazz = Restaurant("trespazz", "asian" )
+print(trespazz.name)
+print(trespazz.cuisine_type)
+
+trespazz.describe_restaurant()
+trespazz.open_restaurant()
+print('*' * 30)
