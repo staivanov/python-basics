4631308ae2e9a17cb7e78e482b80256794fb2436
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-24 11:19:26 +0300

    Restaurant and user classes were updated.

--- 9.classes/restaurant.py
+++ 9.classes/restaurant.py
@@ -1,9 +1,11 @@
 class Restaurant:
     """Represent a restaurant class with his name and cuisine food type."""
-    def __init__(self, name, cuisine_type):
+
+    def __init__(self, name, cuisine_type, number_served=0):
         """ Initialize name and cuisine type for the current instance. """
         self.name = name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
 
     def describe_restaurant(self):
         """ A simple function that prints all information about the restaurant current instance. """
@@ -12,3 +14,11 @@ class Restaurant:
     def open_restaurant(self):
         """ A simple function that prints a welcome message to all on the console. """
         print(f"Welcome to {self.name}!")
+
+    def set_number_served(self, portions):
+        """ This function set the number of served portions at the current restaurant instance."""
+        self.number_served = portions
+
+    def increment_number_served(self, portions_to_add):
+        """ This function add user defined number of served portions to the current served portions on the restaurant instance."""
+        self.number_served += portions_to_add
--- 9.classes/user.py
+++ 9.classes/user.py
@@ -1,17 +1,27 @@
 class User:
 
-    def __init__(self, first_name, last_name, username, gender, age):
-        """Initialize a new user with his first name, last name, username, gender and age."""
+    def __init__(self, first_name, last_name, username, gender, age, login_attempts=0):
+        """Initialize a new user with his first name, last name, username, gender, age and login attempts."""
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.gender = gender
         self.age = age
+        self.login_attempts = login_attempts
 
     def describe_user(self):
         """ A function that prints on the console all attributes with their values about the current instance of the user. """
-        print(f" First name: {self.first_name}\n Last name: {self.last_name}\n Username: {self.username}\n Gender: {self.gender}\n Age: {self.age}\n")
+        print(
+            f" First name: {self.first_name}\n Last name: {self.last_name}\n Username: {self.username}\n Gender: {self.gender}\n Age: {self.age}\n")
 
     def greet_user(self):
         """ A simple greeting message for the current instance on user"""
         print(f"Hola, {self.username}!")
+
+    def increment_login_attempts(self):
+        """ This function increments with one logging attempts to the current user."""
+        self.login_attempts += 1
+
+    def reset_logging_attempts(self):
+        """ This function reset logging attempts to the current user."""
+        self.login_attempts = 0
