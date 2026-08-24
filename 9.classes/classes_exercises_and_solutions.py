e14d9c8135b1a421aef0f9b5ef0a8d3d3c0cc757
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-24 11:19:47 +0300

    More solutions to the exercises.

--- 9.classes/classes_exercises_and_solutions.py
+++ 9.classes/classes_exercises_and_solutions.py
@@ -2,7 +2,7 @@ from restaurant import Restaurant
 from user import User
 
 # 9.1 Restaurant demo.
-trespazz = Restaurant("trespazz", "asian" )
+trespazz = Restaurant("trespazz", "asian")
 print(trespazz.name)
 print(trespazz.cuisine_type)
 
@@ -10,14 +10,22 @@ trespazz.describe_restaurant()
 trespazz.open_restaurant()
 print('*' * 30)
 
-# 9.2 Three restaurants
-ace = Restaurant("ace", "bulgarian" )
-porky = Restaurant("porky", "meat bbq" )
-trespazz = Restaurant("trespazz", "asian" )
+# 9.2, 9.4 Three restaurants
+ace = Restaurant("ace", "bulgarian")
+porky = Restaurant("porky", "meat bbq")
+trespazz = Restaurant("trespazz", "asian")
 
 ace.describe_restaurant()
 porky.describe_restaurant()
 trespazz.describe_restaurant()
+balkan = Restaurant("Balkan", "bulgarian food")
+print(f"In restaurant \"{balkan.name}\" current served portions are {balkan.number_served}.")
+balkan.served_portion = 110
+print(f"In restaurant \"{balkan.name}\" current served portions are {balkan.number_served}.")
+balkan.set_number_served(40)
+print(f"In restaurant \"{balkan.name}\" current served portions are {balkan.number_served}.")
+balkan.increment_number_served(13)
+print(f"In restaurant \"{balkan.name}\" current served portions are {balkan.number_served}.")
 
 print('*' * 30)
 
@@ -25,3 +33,9 @@ print('*' * 30)
 Elga = User("Elga", "Ghironi", "elgaghironi77", "female", 49)
 Elga.describe_user()
 Elga.greet_user()
+Elga.increment_login_attempts()
+Elga.increment_login_attempts()
+Elga.increment_login_attempts()
+print(f"Logging attempts of user {Elga.username} are {Elga.login_attempts}.")
+Elga.reset_logging_attempts()
+print(f"Logging attempts of user {Elga.username} are {Elga.login_attempts}.")
