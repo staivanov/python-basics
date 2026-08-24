c7be93db806107ad1c4f4707a404fc2b29a0bad5
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-24 11:18:35 +0300

    Classes demo was updated with more code.

--- 9.classes/classes_demo.py
+++ 9.classes/classes_demo.py
@@ -1,5 +1,6 @@
 from dog import Dog
 from restaurant import Restaurant
+from car import Car
 
 # Dog demo.
 my_dog = Dog("Dark Lord", 8, "male")
@@ -12,10 +13,15 @@ print('*' * 30)
 
 # Restaurant demo.
 
-trespazz = Restaurant("trespazz", "asian" )
+trespazz = Restaurant("trespazz", "asian")
 print(trespazz.name)
 print(trespazz.cuisine_type)
 
 trespazz.describe_restaurant()
 trespazz.open_restaurant()
 print('*' * 30)
+
+# Car demo.
+
+my_new_car = Car("Porsche", "911 Carrera", 2005)
+print("My new car is " + my_new_car.get_description())
