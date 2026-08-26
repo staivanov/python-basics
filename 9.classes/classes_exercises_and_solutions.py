46fe0a43a73991078afa489a687ec564dac8b55c
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-26 16:16:42 +0300

    New solutions to exercises.

--- 9.classes/classes_exercises_and_solutions.py
+++ 9.classes/classes_exercises_and_solutions.py
@@ -1,5 +1,8 @@
 from restaurant import Restaurant
 from user import User
+from ice_cream_stand import IceCreamStand
+from admin import Admin
+from privileges import Privileges
 
 # 9.1 Restaurant demo.
 trespazz = Restaurant("trespazz", "asian")
@@ -29,7 +32,7 @@ print(f"In restaurant \"{balkan.name}\" current served portions are {balkan.numb
 
 print('*' * 30)
 
-# 9.3 Users
+# 9.3, 9.5 Users
 Elga = User("Elga", "Ghironi", "elgaghironi77", "female", 49)
 Elga.describe_user()
 Elga.greet_user()
@@ -39,3 +42,18 @@ Elga.increment_login_attempts()
 print(f"Logging attempts of user {Elga.username} are {Elga.login_attempts}.")
 Elga.reset_logging_attempts()
 print(f"Logging attempts of user {Elga.username} are {Elga.login_attempts}.")
+
+list_of_privileges = ["can add post", "can delete post", "can ban user", "can add/update/delete user"]
+admin_privileges = Privileges(list_of_privileges)
+admin = Admin("Zahari", "Styoanov",
+              "zaho", "male",
+               33, 0,
+               Privileges(list_of_privileges))
+print(f"All privileges on user @{admin.username} are as follows: ")
+admin.show_privileges()
+print('*' * 30)
+
+# 9.6 Ice Cream Stand
+ice_cream_flavors = ["vanilla", "chocolate", "strawberry", "blackberries", ]
+my_local_stand = IceCreamStand("Palauzovo", "ice cream", 30,ice_cream_flavors )
+my_local_stand.print_all_icecream_flavors()
