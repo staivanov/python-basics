74138d8979f728ff420b73640754adebd42a1b5e
staivanov <40460292+staivanov@users.noreply.github.com>
2026-08-27 15:11:58 +0300

    A new function that draws elements in a random positions from the provided source was defined. Alongside with that is a simple demo.

--- 9.classes/classes_exercises_and_solutions.py
+++ 9.classes/classes_exercises_and_solutions.py
@@ -3,6 +3,8 @@ from user import User
 from ice_cream_stand import IceCreamStand
 from admin import Admin
 from privileges import Privileges
+from dice import Die
+from random import randint
 
 # 9.1 Restaurant demo.
 trespazz = Restaurant("trespazz", "asian")
@@ -47,13 +49,53 @@ list_of_privileges = ["can add post", "can delete post", "can ban user", "can ad
 admin_privileges = Privileges(list_of_privileges)
 admin = Admin("Zahari", "Styoanov",
               "zaho", "male",
-               33, 0,
-               Privileges(list_of_privileges))
+              33, 0,
+              Privileges(list_of_privileges))
 print(f"All privileges on user @{admin.username} are as follows: ")
 admin.show_privileges()
 print('*' * 30)
 
 # 9.6 Ice Cream Stand
 ice_cream_flavors = ["vanilla", "chocolate", "strawberry", "blackberries", ]
-my_local_stand = IceCreamStand("Palauzovo", "ice cream", 30,ice_cream_flavors )
+my_local_stand = IceCreamStand("Palauzovo", "ice cream", 30, ice_cream_flavors)
 my_local_stand.print_all_icecream_flavors()
+print('*' * 30)
+
+# 9.13 Dice. Make a new class Die with attribute sides and method roll_die. Call it 10 times.
+new_die = Die(6)
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+new_die.roll_die()
+
+print('*' * 30)
+
+# 9.14 Lottery. Make a lottery source represented with tuple with 10 numbers and 5 letters. Randomly select 4 items from the tuple representing a winner ticket.
+lottery_source = (78, 'M', 79, 'S', 63, 'M', 99, 'I', 99, 'K', 25, 48, 11, 53, 74)
+
+
+def draw_a_winning_combination(source: tuple):
+    """ This function is a simple random generator of items from a tuple as a source. Evey element can be drawn more than once."""
+    min_random_number = 0
+    max_random_number = int(len(source)) - 1
+    first_draw = 1
+    last_draw = 4
+    winning_ticket_combo = []
+
+    while first_draw <= last_draw:
+        current_random_index = randint(min_random_number, max_random_number)
+        winning_ticket_combo.append(source[current_random_index])
+        first_draw += 1
+
+    return winning_ticket_combo
+
+
+winning_ticket_combination = ()
+winning_ticket_combination = draw_a_winning_combination(lottery_source)
+print(winning_ticket_combination)
